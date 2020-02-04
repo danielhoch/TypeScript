659f0fcf164acd4dae7c4009f26513561c232dfa
Daniel Herbert Hoch <danielhoch28@gmail.com>
2020-02-03 22:47:40 -0300

    Deletando postas desnecessárias

--- 2-AluraBank/ts_node/js/local.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = require("fs");
-fs_1.writeFile('teste.txt', 'Gravei no arquivo', err => {
-    if (err) {
-        return console.log('Não foi possível criar o arquivo');
-    }
-    console.log('arquivo criado com sucesso');
-});
--- 2-AluraBank/ts_node/ts/local.ts
+++
@@ -1,7 +0,0 @@
-import { writeFile } from 'fs';
-writeFile('teste.txt', 'Gravei no arquivo', err => {
-    if(err) {
-        return console.log('Não foi possível criar o arquivo');
-    }
-    console.log('arquivo criado com sucesso');
-});
